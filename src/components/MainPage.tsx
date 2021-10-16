e3755bf6fde14a4b96f228e4cb1f129805717aec
cthacker-udel <cthacker@udel.edu>
2021-10-16 12:57:13 -0400

    update 2

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,44 +1,67 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import { SemesterTable } from './SemesterTable';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { CourseContainer } from './CourseContainer';
 
-export const MainPage = (): JSX.Element =>
+export const MainPage = (): JSX.Element => {
+	const onBeforeCapture = useCallback(() => {
+		console.log('before capture');
+	}, []);
 
-	<DragDropContext
-		onBeforeCapture={() => console.log('before capture')}
-		onBeforeDragStart={() => console.log('before drag start')}
-		onDragStart={() => console.log('ondrag start')}
-		onDragUpdate={() => console.log('ondrag update')}
-		onDragEnd={() => console.log('on drag end')}
-	>
-		<Container>
-			<Row>
-				<Col>
-					Course Scheduler
-				</Col>
-				<Col>
-					<Row>
-						<Col>
-							{/* concentration list */}
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							{/* reqs */}
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<CourseContainer />
-				</Col>
-				<Col>
-					<SemesterTable />
-				</Col>
-			</Row>
-		</Container>
-	</DragDropContext>;
+	const onBeforeDragStart = useCallback(() => {
+		console.log('before drag start');
+	}, []);
+
+	const onDragStart = useCallback(() => {
+		console.log('on drag start');
+	}, []);
+
+	const onDragUpdate = useCallback(() => {
+		console.log('on drag update');
+	}, []);
+
+	const onDragEnd = useCallback(() => {
+		console.log('on drag end');
+	}, []);
+
+
+	return (
+		<DragDropContext
+			onBeforeCapture={onBeforeCapture}
+			onBeforeDragStart={onBeforeDragStart}
+			onDragStart={onDragStart}
+			onDragUpdate={onDragUpdate}
+			onDragEnd={onDragEnd}
+		>
+			<Container>
+				<Row>
+					<Col>
+						Course Scheduler
+					</Col>
+					<Col>
+						<Row>
+							<Col>
+								{/* concentration list */}
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								{/* reqs */}
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<CourseContainer />
+					</Col>
+					<Col>
+						<SemesterTable />
+					</Col>
+				</Row>
+			</Container>
+		</DragDropContext>
+	);
+};
